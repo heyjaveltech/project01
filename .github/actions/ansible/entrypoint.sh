940ce650e84c779b0c0d4a01490433d4ed256cc0
Javel Wilson <heyjavel.tech@gmail.com>
2022-06-29 23:08:29 +0000

    New commit

--- .github/actions/ansible/entrypoint.sh
+++ .github/actions/ansible/entrypoint.sh
@@ -3,7 +3,11 @@
 
 echo $ANSIBLE_VAULT_PASSWORD >> .vault
 
-ansible-playbook -i hosts.yml site.yml --vault-password-file .vault
-
-#avoids locally storing on a mounted volume 
-rm .vault
+if ! ansible-playbook -i hosts_azure_rm.yml site.yml --vault-password-file .vault; 
+then
+  echo "Ansible failed!" 
+  rm .vault
+  exit 1
+else
+  rm .vault
+fi
